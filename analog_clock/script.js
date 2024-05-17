6bd83d8addb5e742d11583f65a7ff3e078f0c1ce
Victor Panguene <svpanguene@gmail.com>
2024-05-17 23:58:52 +0200

    First commit

---
+++ analog_clock/script.js
@@ -0,0 +1,4 @@
+// Getting all elements from the HTML document
+const secondsHand = document.getElementById("seconds-hand");
+const minutesHand = document.getElementById("minutes-hand");
+const hoursHand = document.getElementById("hours-hand");
