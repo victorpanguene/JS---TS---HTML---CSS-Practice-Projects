66bdc24a85e29e3fd3e53caf5d229329f6353b38
Victor Panguene <svpanguene@gmail.com>
2024-05-18 00:57:10 +0200

    time pointers rotating

--- analog_clock/script.js
+++ analog_clock/script.js
@@ -2,3 +2,23 @@
 const secondsHand = document.getElementById("seconds-hand");
 const minutesHand = document.getElementById("minutes-hand");
 const hoursHand = document.getElementById("hours-hand");
+
+console.log(
+  `Seconds: ${secondsHand}, Minutes: ${minutesHand}, Hours: ${hoursHand}`
+);
+
+// Get the time function
+const getTime = () => {
+  const now = new Date();
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  // Rotating time pointers
+  const timeInterval = 6;
+  secondsHand.style.transform = "rotate(" + seconds * timeInterval + "deg)";
+  minutesHand.style.transform = "rotate(" + minutes * timeInterval + "deg)";
+  hoursHand.style.transform = "rotate(" + (hours * 30 + minutes / 2) + "deg)";
+};
+setInterval(getTime, 100);
