2bf4f1135086da6f57c998db14e5b50859626904
Victor Panguene <svpanguene@gmail.com>
2024-05-25 18:37:29 +0200

    attempt to dynamic houra

--- digital_clock/script.js
+++ digital_clock/script.js
@@ -4,9 +4,16 @@ const seconds = document.getElementsByClassName("seconds");
 
 const getTime = () => {
   const currentTime = new Date();
-  const hourUpdate = (hours.innerHTML = currentTime.minutes + 1);
+  const hourUpdate = currentTime.getHours();
+  hours.textContent = hourUpdate.toString();
   console.log(hourUpdate);
 };
-const pointsCounter = () => {};
 
-console.log(hours[0].textContent + 1, minutes, seconds);
+getTime();
+const pointsCounter = () => {
+  const points = document.getElementsByClassName("show");
+  console.log(points[1].className);
+};
+pointsCounter();
+
+// setInterval(pointsCounter, 1000);
