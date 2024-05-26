eef3a79b8af961f1c4a2769a5088d6c4531a5214
Victor Panguene <svpanguene@gmail.com>
2024-05-26 19:52:26 +0200

    temptation on functions timw

--- digital_clock/script.js
+++ digital_clock/script.js
@@ -1,19 +1,19 @@
-const hours = document.getElementsByClassName("hours");
+let hours = document.getElementsByClassName("hours");
 const minutes = document.getElementsByClassName("minutes");
 const seconds = document.getElementsByClassName("seconds");
 
 const getTime = () => {
   const currentTime = new Date();
   const hourUpdate = currentTime.getHours();
-  hours.textContent = hourUpdate.toString();
-  console.log(hourUpdate);
+  const minutesUpdate = currentTime.getMinutes();
+  const secondsUpdate = currentTime.getSeconds();
+  const time = hours.innerHTML();
+  console.log("🚀 ~ getTime ~ time:", time);
+  console.log("🚀 ~ getTime ~ hourUpdate:", hourUpdate);
+
+  return hourUpdate;
 };
 
 getTime();
-const pointsCounter = () => {
-  const points = document.getElementsByClassName("show");
-  console.log(points[1].className);
-};
-pointsCounter();
 
 // setInterval(pointsCounter, 1000);
