b91e1da9fccee825bc0d92740d0ed5717a054f62
Victor Panguene <svpanguene@gmail.com>
2024-06-16 22:37:35 +0200

    check winner

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -27,6 +27,18 @@ function addGo(e) {
 
 function checkScore() {
   const allSquares = document.querySelectorAll('.square')
-  console.log("🚀 ~ checkScore ~ allSquares:", allSquares)
+  const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0,4,8], [2,4,6]
+  ]
+  winningCombos.forEach(array => {
+    const circleWins = array.every(cell => {
+      allSquares[cell].firstChild?.classList.contains('circle')
+    if (circleWins) {
+      infoDisplay.textContent = "Circle Wins"
+    }
+    })
+  })
 }
 checkScore()
