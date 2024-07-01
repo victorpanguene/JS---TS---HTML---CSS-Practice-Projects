5f65adc61f4c1c349d49732907d77fff5cd8d17b
Victor Panguene <svpanguene@gmail.com>
2024-07-01 23:36:11 +0200

    game works fine

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,34 +1,36 @@
-const gameBoard = document.querySelector('#gameboard')
-const infoDisplay = document.querySelector('#info')
-const startCells = ['', '', '', '', '', '', '', '', '']
+const gameBoard = document.querySelector("#gameboard")
+const infoDisplay = document.querySelector("#info")
+const startCells = ["", "", "", "", "", "", "", "", ""]
 
-infoDisplay.textContent = 'Circle goes first'
+infoDisplay.textContent = "Circle goes first"
 
-let go = 'circle'
+let go = "circle"
 
 function createBoards(){ 
  startCells.forEach((_cell, index) => {
-  const cellElement = document.createElement('div')
-  cellElement.classList.add('square')
+  const cellElement = document.createElement("div")
+  cellElement.classList.add("square")
   cellElement.id = index
- cellElement.addEventListener('click', addGo)
- gameBoard.append(cellElement)
+  cellElement.addEventListener("click", addGo)
+  gameBoard.append(cellElement)
 })
 }
-  
+   
 createBoards() 
   
 function addGo(e) {
-  console.log('Please', e)
-  const goDisplay = document.createElement('div')
-  goDisplay.classList.add(go)
+  console.log(e)
+  const goDisplay = document.createElement("div")
+  goDisplay.classList.add(go)   
   e.target.append(goDisplay)
-  go = go === 'circle' ? "cross" : 'circle'
-  e.target.removeEventListener('click', addGo)
+  go = go === "circle" ? "cross" : "circle"
+  infoDisplay.textContent = "Now it is " + go + " time"
+  e.target.removeEventListener("click", addGo)
+  checkScore()
 }  
 
-function checkScore() {
-  const allSquares = document.querySelectorAll('.square')
+function checkScore() {  
+  const allSquares = document.querySelectorAll(".square")
   const winningCombos = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
@@ -37,10 +39,20 @@ function checkScore() {
   
   winningCombos.forEach(array => {
     const circleWins = array.every(cell =>
-      allSquares[cell].firstChild?.classList.contains('circle'))
-     if (circleWins) {
-      infoDisplay.textContent = 'Circle wins'
+      allSquares[cell].firstChild?.classList.contains("circle"))
+    if (circleWins) {
+      infoDisplay.textContent = "Circle wins"
+      allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
     }
   })
+
+    winningCombos.forEach(array => {
+    const crossWins = array.every(cell =>
+      allSquares[cell].firstChild?.classList.contains("cross"))
+      if (crossWins) {
+        infoDisplay.textContent = "Cross wins"
+        allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+      }
+  })
 }
-checkScore()
+
