4a7b129ac7bf6698872995f6eb9dad570465f083
Victor Panguene <svpanguene@gmail.com>
2024-07-08 23:08:09 +0200

    Add concept 1

--- tic-tac-toe/concepts.js
+++ tic-tac-toe/concepts.js
@@ -3,6 +3,7 @@
 * display flex properties
 * CSS after and before properties
 * array.every every is a method 
+* every returns true if every element in the array passes the criteria, and * * *false otherwise. every will stop looking and return false if it finds an *element that doesn’t match the criteria; otherwise, it will have to scan the *entire array:
 * cloneNode
- 
+
 */
