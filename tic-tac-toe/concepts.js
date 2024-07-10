3f9239d6a90a9de35cd799716ec8639af185ecbe
Victor Panguene <52853174+victorpanguene@users.noreply.github.com>
2024-07-10 22:15:12 +0200

    Update concepts.js

--- tic-tac-toe/concepts.js
+++ tic-tac-toe/concepts.js
@@ -6,4 +6,5 @@
 * every returns true if every element in the array passes the criteria, and * * *false otherwise. every will stop looking and return false if it finds an *element that doesn’t match the criteria; otherwise, it will have to scan the *entire array:
 * cloneNode
 
+
 */
