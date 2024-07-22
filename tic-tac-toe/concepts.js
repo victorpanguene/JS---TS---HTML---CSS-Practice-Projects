73f6ee09511abbde3839aee666602936b0e6ab38
Victor Panguene <52853174+victorpanguene@users.noreply.github.com>
2024-07-22 23:08:37 +0200

    Update concepts.js

--- tic-tac-toe/concepts.js
+++ tic-tac-toe/concepts.js
@@ -5,5 +5,4 @@
 * array.every every is a method 
 * every returns true if every element in the array passes the criteria, and * * *false otherwise. every will stop looking and return false if it finds an *element that doesn’t match the criteria; otherwise, it will have to scan the *entire array:
 * cloneNode
-
 */
